0b0099c0276c26d1b0e67f9755e77e526921836d
Angel Rodriguez <aleangrodriguez123@gmail.com>
2017-02-26 16:56:17 -0500

    Añadiendo Firebase Database

--- components/App.jsx
+++ components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import firebase from 'firebase';
 
 class Profile extends React.Component{
 	constructor(){
@@ -17,46 +18,72 @@ class App extends React.Component {
 
 	constructor() {
 		super();
-		this.state = {
-			list : [
-				{
-					name:"Producto 1",
-					description: "Description 1"
-				},
-				{
-					name:"Producto 2",
-					description: "Description 1"
-				}
+		var config = {
+			apiKey: "AIzaSyBaBgGKOx-o818zHDnaCaLRaVgXhU21BPw",
+			authDomain: "personalhosting-d5cd6.firebaseapp.com",
+			databaseURL: "https://personalhosting-d5cd6.firebaseio.com",
+			storageBucket: "personalhosting-d5cd6.appspot.com",
+			messagingSenderId: "750940164737"
+		};
+		firebase.initializeApp(config);
 
-			]
+		this.state = {
+			list:[]
 		}
+
+	}
+
+	componentWillMount(){
+		this.firebaseRef = firebase.database().ref('products');
+		this.firebaseRef.on("child_added", function(dataSnapshot) {
+			let product = dataSnapshot.val();
+			let list = this.state.list.slice();
+			product.id = dataSnapshot.key;
+			list.push(product);
+			this.setState({
+				list:list
+			})
+
+		}.bind(this));
+	}
+
+	componentWillUnmount() {
+  	this.firebaseRef.off();
+	}
+
+	deleteProduct(key){
+		let product = this.firebaseRef.child(key)
+		product.ref.remove();
 	}
 
 
 	handleClick(){
-		const list = this.state.list.slice();
-		list.push({
-			name:"Producto 1",
-			description: "Description 1"
-		});
-		this.setState({
-			list:list
-		})
+		// const list = this.state.list.slice();
+		let data = {
+			name: "Producto 2",
+			description: "Description",
+			date: new Date()
+		}
+		// list.push(data);
+		this.firebaseRef.push(data);
+		// this.setState({list: list});
 	}
 
    render() {
       return (
+				<div className="container">
 				 <div className="row">
 					 <div className="col-md-12">
-						 <h1>Lista de mercado</h1>
+						 <h1>Mi lista de mercado</h1>
 					 </div>
 					 <div className="col-md-6">
-						 <ListGrocery handleClick={()=> this.handleClick()} items={this.state.list} />
+						 <ListGrocery handleClick={()=> this.handleClick()} items={this.state.list} deleteProduct = {(product) => this.deleteProduct(product)}  />
 					 </div>
 					 <div className="col-md-6">
 						 <a onClick={()=> this.handleClick() } className="btn btn-primary">Add new grocery</a>
 					 </div>
 				 </div>
+			 </div>
       );
    }
 }
@@ -66,7 +93,7 @@ class ListGrocery extends React.Component{
 	render(){
 		return(
 			<div className="list-group">
-				{this.props.items.map(item => <Grocery grocery={item}  /> )}
+				{this.props.items.map(item => <Grocery grocery={item} deleteProduct = { () => this.props.deleteProduct(item.key) }  /> )}
 			</div>
 		);
 	}
@@ -80,9 +107,12 @@ class Grocery extends React.Component{
 				<div className="col-md-2">
 					<img src="http://www.imagenesdeamor.pro/wp-content/uploads/2016/09/imagenes-con-frases-de-desamor-8-320x320.jpg" className="img-responsive" width="100" height="100"></img>
 				</div>
-				<div className="col-md-10">
+				<div className="col-md-7">
 					<h4 className="list-group-item-heading">{this.props.grocery.name}</h4>
-				<p className="list-group-item-text">{this.props.grocery.description}</p>
+					<p className="list-group-item-text">{this.props.grocery.description}</p>
+				</div>
+				<div className="col-md-3">
+					<button onClick={ () => this.props.deleteProduct()} className="btn btn-danger">Eliminar</button>
 				</div>
 			</div>
 		</a>
